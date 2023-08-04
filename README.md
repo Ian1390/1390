608281f44c16967a66c7e870788454425bd269da
Ian1390 <141403527+Ian1390@users.noreply.github.com>
2023-08-04 15:31:40 +0700

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open this page at [https://ian1390.github.io/1390/](https://ian1390.github.io/1390/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/ian1390/1390** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/ian1390/1390** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,13 @@
+if (mbit_Robot.Line_Sensor(mbit_Robot.enPos.LeftState, mbit_Robot.enLineState.White) && mbit_Robot.Line_Sensor(mbit_Robot.enPos.RightState, mbit_Robot.enLineState.White)) {
+    mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Run, 50)
+} else if (mbit_Robot.Line_Sensor(mbit_Robot.enPos.LeftState, mbit_Robot.enLineState.White) && mbit_Robot.Line_Sensor(mbit_Robot.enPos.RightState, mbit_Robot.enLineState.Black)) {
+    mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Run, 60)
+} else if (mbit_Robot.Line_Sensor(mbit_Robot.enPos.LeftState, mbit_Robot.enLineState.Black) && mbit_Robot.Line_Sensor(mbit_Robot.enPos.RightState, mbit_Robot.enLineState.White)) {
+    mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Run, 60)
+} else if (mbit_Robot.Line_Sensor(mbit_Robot.enPos.LeftState, mbit_Robot.enLineState.Black) && mbit_Robot.Line_Sensor(mbit_Robot.enPos.RightState, mbit_Robot.enLineState.Black)) {
+    mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_Stop)
+    mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Red)
+    basic.pause(5000)
+} else {
+    mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_Stop)
+}
